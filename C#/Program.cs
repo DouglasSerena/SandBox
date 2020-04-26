2d7b0c9a1dbd8db7f2e7f92831168e8c434b5eba
DouglasSerena <douglas222serena@rede.ulbra.br>
2020-04-26 20:16:20 -0300

    update

--- C#/Program.cs
+++ C#/Program.cs
@@ -3,14 +3,14 @@
 namespace SandBox {
     class Program {
         static void Main(string[] args) {
-            int numbler1, numbler2;
-            float resposta;
+            //int numbler1, numbler2;
+            //float resposta;
             Console.WriteLine("Digite um numero : ");
-            numbler1 = Console.ReadLine(int);
+            //numbler1 = Console.ReadLine(int);
             Console.WriteLine("Digite outro numero : ");
-            numbler2 = Console.ReadLine();
-            resposta = numbler1 + numbler2;
-            Console.WriteLine("Sua soma deu " + resposta);
+            //numbler2 = Console.ReadLine();
+            //resposta = numbler1 + numbler2;
+            //Console.WriteLine("Sua soma deu " + resposta);
 
             Console.ReadKey();
         }
